e730b5f5f6972f357a056676358a6c225ee6b2b7
Luccas Diazzi <diazziti@gmail.com>
2026-09-02 11:23:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,5 +13,5 @@ init_db()
 app.register_blueprint(user_bp, url_prefix='/user')
 app.register_blueprint(formulario_bp, url_prefix='/formularios')
 
-if__name__ == '__main__'
+if__name__ == '__main__':
     app.run(debug=True)
